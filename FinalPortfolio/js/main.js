bc97ff20713eea62630c02dcbc2b526de2e1f32d
Marco Weibel <marco.o.weibel@gmail.com>
2015-05-06 18:14:37 -0400

    added some interactions to sort portfolio files

--- FinalPortfolio/js/main.js
+++ FinalPortfolio/js/main.js
@@ -1,17 +1,47 @@
-//switch from normal header to small header when scrolling down.
+//switch from normal header to small header size when scrolling down.
 $(window).scroll(function(){
-    if($(document).scrollTop() > 0) {
+    if($(document).scrollTop() > 5) {
         $('.header').addClass('small'); 
         $('.header__logo img').addClass('small');
         $('.header__nav ul').addClass('small');
+        $('.header__nav ul ul').addClass('small');
+        $('.portfolioFilter').addClass('small');
         $( ".header__info" ).addClass('is-hidden');
-        $('.content').addClass('small'); 
     } else {
         $('.header').removeClass('small');
         $('.header__logo img').removeClass('small');
         $('.header__nav ul').removeClass('small');
+        $('.header__nav ul ul').removeClass('small');
+        $('.portfolioFilter').removeClass('small');
         $( ".header__info" ).removeClass('is-hidden');
-        $('.content').removeClass('small');
     }
 });
 
+/*I got help from an Isotope tutorial for this code */
+            $(window).load(function(){
+                var $container = $('.portfolioContainer');
+                $container.isotope({
+                    filter: '*',
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+             
+                $('.portfolioFilter a').click(function(){
+                    $('.portfolioFilter .current').removeClass('current');
+                    $(this).addClass('current');
+             
+                    var selector = $(this).attr('data-filter');
+                    $container.isotope({
+                        filter: selector,
+                        animationOptions: {
+                            duration: 750,
+                            easing: 'linear',
+                            queue: false
+                        }
+                     });
+                     return false;
+                }); 
+            });
